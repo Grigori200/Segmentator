ac461b4728eb70829b8ce23f95104d599e63dc6c
Grigori200 <mateusz.grzesiuk@op.pl>
2022-11-14 13:07:47 +0100

    cleaned

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import os
 
-from segmentation import full_segmentation, segmentation_without_diarization, sole_limitation
+from segmentation import sole_limitation
 import pandas as pd
 
 
 def main():
-    # first = full_segmentation(os.path.join('data', '2007100212545620.wav'), 'outputs', 30., 1.)
-    # second = full_segmentation(os.path.join('data', 'a1.wav'), 'outputs', 30., 1.)
-    # third = full_segmentation(os.path.join('data', 'a2.wav'), 'outputs', 30., 1.)
-    # fourth = full_segmentation(os.path.join('data', 'Jeziorany_odc1.wav'), 'outputs', 30., 1.)
-    fifth = sole_limitation(os.path.join('data', 'odc2.wav'), 'outputs', 30., 1.5)
-    # print(pd.read_csv(os.path.join(first, 'timestamps.csv'), index_col=0))
-    # print(pd.read_csv(os.path.join(second, 'timestamps.csv'), index_col=0))
-    # print(pd.read_csv(os.path.join(third, 'timestamps.csv'), index_col=0))
-    # print(pd.read_csv(os.path.join(fourth, 'timestamps.csv'), index_col=0))
+    fifth = sole_limitation(os.path.join('data', 'odc2.wav'), 'outputs', 30.)
     print(pd.read_csv(os.path.join(fifth, 'timestamps.csv'), index_col=0))
 
 
--- segmentation.py
+++ segmentation.py
@@ -1,106 +1,13 @@
-from inaSpeechSegmenter import Segmenter
 import pandas as pd
 from typing import List
-from pyannote.pipeline.typing import PipelineOutput
 import os
-from pyannote.audio import Pipeline
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 from tqdm import tqdm
 
-TMP_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
-PIPELINE = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
-                                    use_auth_token='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')
-
-
-def segmentation_to_df(segmentation: List) -> pd.DataFrame:
-    df = pd.DataFrame(columns=['speaker', 'start', 'finish'])
-    for segment in tqdm(segmentation, 'segmentation to dataframe'):
-        speaker, start, end = segment
-        df = pd.concat([df, pd.DataFrame({'speaker': speaker, 'start': start, 'finish': end}, index=[0])],
-                       ignore_index=True)
-    return df
-
-
-def diarization_to_df(diarization: PipelineOutput) -> pd.DataFrame:
-    diarized_df = pd.DataFrame(columns=['start', 'finish', 'speaker'])
-    for turn, speaker, _ in tqdm(diarization.itertracks(yield_label=True), 'diarization to data frame'):
-        diarized_df = pd.concat(
-            [diarized_df, pd.DataFrame({'start': turn.start, 'finish': turn.end, 'speaker': speaker}, index=[0])],
-            ignore_index=True)
-    return diarized_df
-
-
-def cut_gaps_on_borders(df: pd.DataFrame, gap_labels: List) -> pd.DataFrame:
-    start_index = df[~df['speaker'].isin(gap_labels)].index[0]
-    end_index = df[~df['speaker'].isin(gap_labels)].index[-1] + 1
-    return df.iloc[start_index:end_index].reset_index(drop=True)
-
-
-def merge_speakers(df: pd.DataFrame, speaker_labels: List, new_label: str) -> pd.DataFrame:
-    tmp_df = pd.DataFrame(columns=['start', 'finish', 'speaker'])
-    prev_speaker = False
-    for index, row in tqdm(df.iterrows(), 'merging speakers'):
-        curr_speaker = row['speaker'] in speaker_labels
-        if curr_speaker and prev_speaker:
-            tmp_df.loc[tmp_df.index[-1], 'finish'] = row['finish']
-        else:
-            tmp_df = pd.concat([tmp_df,
-                                pd.DataFrame({'start': row['start'], 'finish': row['finish'], 'speaker': row['speaker']},
-                                             index=[0])], ignore_index=True)
-        prev_speaker = curr_speaker
-    tmp_df.loc[tmp_df['speaker'].isin(speaker_labels), 'speaker'] = new_label
-    return tmp_df
-
-
-def remove_speakers(df: pd.DataFrame, speakers: List[str or int]) -> pd.DataFrame:
-    return df.drop(df[df['speaker'].isin(speakers)].index)
-
 
 def calculate_length(df: pd.DataFrame) -> pd.DataFrame:
     df['length'] = df['finish'] - df['start']
-    return df
-
-
-def save_segments(source_path: str, df: pd.DataFrame, destination_folder: str, save_csv: bool = False) -> pd.DataFrame:
-    if not os.path.exists(destination_folder):
-        os.makedirs(destination_folder)
-    part_df = pd.DataFrame(columns=['path', 'start', 'finish'])
-    audio = AudioSegment.from_wav(source_path)
-    core_name = os.path.basename(os.path.normpath(source_path.split('.')[0]))
-    format = source_path.split('.')[-1]
-    for index, segment in tqdm(df.iterrows(), 'saving segments'):
-        file_path = os.path.join(destination_folder, core_name + f'_{index}.{format}')
-        part_df = pd.concat([part_df, pd.DataFrame(
-            {'path': file_path, 'start': segment['start'], 'finish': segment['finish']},
-            index=[0])],
-                            ignore_index=True, copy=False)
-        audio[segment.loc['start'] * 1000:segment.loc['finish'] * 1000].export(file_path, format=format)
-    if save_csv:
-        part_df.to_csv(os.path.join(destination_folder, core_name + '.csv'))
-    return part_df
-
-
-def segmentation(file_path: str, output_folder: str, min_gap: float) -> pd.DataFrame:
-    seg = Segmenter()
-    segments = seg(file_path)
-    df = segmentation_to_df(segments)
-    gaps = ['noEnergy', 'noise', 'music']
-    gap = 'gap'
-    df = merge_speakers(df, gaps, gap)
-    df = add_margin_of_mistake(df, min_gap, [gap])
-    df = remove_speakers(df, [gap])
-    tmp_df = save_segments(file_path, df, output_folder)
-    return tmp_df
-
-
-def add_margin_of_mistake(df: pd.DataFrame, min_gap: float, gaps: List[str]) -> pd.DataFrame:
-    for (index_1, row_1), (index_2, row_2) in zip(df[:-1].iterrows(), df[1:].iterrows()):
-        if row_1['speaker'] not in gaps and row_2['speaker'] in gaps:
-            if row_2['finish'] - row_2['start'] < min_gap:
-                df.at[index_1, 'finish'] = row_2['finish']
-            else:
-                df.at[index_1, 'finish'] = row_1['finish'] + min_gap
     return df
 
 
@@ -117,31 +24,6 @@ def connect_records(df: pd.DataFrame, length_limit: float = 30.) -> pd.DataFrame
             end = row['finish']
     new_df = pd.concat([new_df, pd.DataFrame({'start': start, 'finish': end}, index=[0])], ignore_index=True)
     return new_df
-
-
-def split_simultaneous_speakers(df: pd.DataFrame) -> pd.DataFrame:
-    last_end = 0.
-    for index, row in tqdm(df.iterrows(), 'splitting simultaneous speakers'):
-        if row['finish'] <= last_end != 0 and row['start'] <= last_end:
-            df.loc[index, 'speaker'] = -1
-        df.loc[index, 'start'] = last_end
-        last_end = max(last_end, row['finish'])
-    return remove_speakers(df, [-1])
-
-
-def diarization(file_path: str, output_folder: str, length: float) -> pd.DataFrame:
-    try:
-        segments = PIPELINE(file_path)
-        df = diarization_to_df(segments)
-    except ValueError:
-        df = pd.DataFrame(columns=['start', 'finish', 'speaker'])
-    df = split_simultaneous_speakers(df)
-    if len(df.index) < 1:
-        df = pd.concat(
-            [df, pd.DataFrame({'start': 0., 'finish': length, 'speaker': 'Unknown'}, index=[0])],
-            ignore_index=True)
-    tmp_df = save_segments(file_path, df, output_folder)
-    return tmp_df
 
 
 def prepare_splits(file_path: str, start: float, end: float, min_silence_len: int = 700, silence_thresh: int = -12,
@@ -212,12 +94,6 @@ def limiting_length(df: pd.DataFrame, length_limit: float, base_start: float, ba
     return new_df
 
 
-def delete_tmp_data(df: pd.DataFrame) -> None:
-    for index, row in tqdm(df.iterrows(), 'deleting tmp data'):
-        if os.path.exists(row['path']):
-            os.remove(row['path'])
-
-
 def save_results(source_path: str, df: pd.DataFrame, destination_folder: str) -> str:
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
@@ -240,29 +116,7 @@ def save_results(source_path: str, df: pd.DataFrame, destination_folder: str) ->
     return subfolder
 
 
-def full_segmentation(file_path: str, output_folder: str, length_limit: float, min_gap: float) -> str:
-    full_df = pd.DataFrame(columns=['start', 'finish'])
-    df = segmentation(file_path, TMP_FOLDER, min_gap)
-    for index, row in tqdm(df.iterrows(), 'iteration over segments'):
-        diar_df = diarization(row['path'], TMP_FOLDER, row['finish'] - row['start'])
-        if len(diar_df.index) > 0:
-            limited_df = limiting_length(diar_df, length_limit, row['start'], row['finish'])
-            full_df = pd.concat([full_df, limited_df], ignore_index=True)
-            delete_tmp_data(diar_df)
-    delete_tmp_data(df)
-    full_df = connect_records(full_df)
-    return save_results(file_path, full_df, output_folder)
-
-
-def segmentation_without_diarization(file_path: str, output_folder: str, length_limit: float, min_gap: float) -> str:
-    df = segmentation(file_path, TMP_FOLDER, min_gap)
-    limited_df = limiting_length(df, length_limit, df.at[0, 'start'], df.at[len(df.index)-1, 'finish'])
-    delete_tmp_data(df)
-    full_df = connect_records(limited_df)
-    return save_results(file_path, full_df, output_folder)
-
-
-def sole_limitation(file_path: str, output_folder: str, length_limit: float, min_gap: float) -> str:
+def sole_limitation(file_path: str, output_folder: str, length_limit: float) -> str:
     audio = AudioSegment.from_wav(file_path)
     df = pd.DataFrame({'path': [file_path], 'start': [0.], 'finish': [audio.duration_seconds]})
     limited_df = limiting_length(df, length_limit, 0., audio.duration_seconds)
